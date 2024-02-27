e6888b30713759370ed32ec90b76207020b3fc55
Pratik Jadon <pratikucskmpj@gmail.com>
2024-02-27 13:37:59 +0530

    UserModel Quality_update

--- src/main/java/Bank_RMS/model/user.java
+++ src/main/java/Bank_RMS/model/user.java
@@ -1,15 +1,28 @@
 package Bank_RMS.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
+@Document(collection = "user")
 @Getter @Setter
 public class user {
+    @Id
+    private String id;
+    @NotNull(message = "FirstName cannot be null.")
     private String firstname;
+    @NotNull(message = "LastName cannot be null.")
     private String lastname;
+    @NotNull(message = "Username cannot be null.")
     private String username;
+    @NotNull(message = "AccountType cannot be null.")
     private String AccountType;
+    @NotNull(message = "Password cannot be null.")
     private String password;
-
+    @NotNull(message = "DOB cannot be null.")
+    private Date dob;
 }
